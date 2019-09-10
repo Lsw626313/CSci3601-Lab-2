5231a2c34f8302d5ffcab88f2e84ed0ad5742272
iwata008 <iwata008@morris.umn.edu>
2019-09-10 15:08:19 -0500

    Add a comment

--- server/src/test/java/umm3601/user/FullUserListFromDB.java
+++ server/src/test/java/umm3601/user/FullUserListFromDB.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
-
+// Waller and Machi
 /**
  * Tests umm3601.user.Database listUser functionality
  */
