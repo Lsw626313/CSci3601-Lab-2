5d467330136d1f6ae3b9d08fc28103b7c92faf72
Lsw626313 <lsw626313@gmail.com>
2019-09-12 03:26:28 -0500

    Issue #19 #20 #21 Add client-support for owner filter

--- client/javascript/todos.js
+++ client/javascript/todos.js
@@ -5,3 +5,19 @@ function getAllTodos() {
     document.getElementById('jsonDump').innerHTML = returned_json;
   });
 }
+
+function getTodosLimitedNumber() {
+  console.log("Getting todos with a maximum number.");
+
+  get("/api/todos?limit=" + document.getElementById("limit").value, function (returned_json) {
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+
+function getTodosByOwner() {
+  console.log("Getting todos with a owner.");
+
+  get("/api/todos?owner=" + document.getElementById("owner").value, function (returned_json) {
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
