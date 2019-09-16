9eab5903bbc1c4f40ff5daec2bdb94aa70ebdd2c
Lsw626313 <lsw626313@gmail.com>
2019-09-16 16:04:53 -0500

    finish github action test

--- server/src/test/java/umm3601/todo/FullTodoListFromDB.java
+++ server/src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.HashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class FullTodoListFromDB {
 
   @Test
   public void totalUserCount() throws IOException {
-    fail();
     Database db = new Database("src/main/data/todos.json");
     Todo[] allTodos = db.listTodos(new HashMap<>());
     assertEquals("Incorrect total number of todos", 300, allTodos.length);
